1fe6c34ae2a9a714c4b96a34502e561e6fdd2e03
eminealbayram <albayramemine27@gmail.com>
2021-07-07 18:55:40 +0000

    a new file created with policy

---
+++ IAM/policy.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_policy" "policy" {
+  name        = "admins"
+  path        = "/"
+  description = "admins"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
