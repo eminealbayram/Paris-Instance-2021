49a412d8f7d2c8db8daa0c408368a257095137b2
eminealbayram <albayramemine27@gmail.com>
2021-07-09 20:26:08 +0000

    added comment about count.index + 1 for creating multiple instances

--- EC2-with-Output/ec2.tf
+++ EC2-with-Output/ec2.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "web" {
   key_name                    = aws_key_pair.test_key.key_name
   user_data                   = file("user_data.sh")
   tags = {
-    Name      = "Web ${count.index + 1}"
+    Name      = "Web ${count.index + 1}" #automatically gives names to the instances starting from 1.
     CreatedBy = "Engineer"
     Team      = "DevOps"
   }
