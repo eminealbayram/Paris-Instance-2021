408fe508809f48d1be350a6822b0addd7a7954e8
eminealbayram <albayramemine27@gmail.com>
2021-07-09 19:55:09 +0000

    output was created together with Ec2

--- EC2-with-Output/ec2.tf
+++ EC2-with-Output/ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
-  ami                         = "ami-0277b52859bac6f4b"
+  ami                         = "ami-062fdd189639d3e93" #copied from Amazon from first window when we want to create a new instance.
   instance_type               = "t3.micro"
   count                       = 3
-  availability_zone           = "us-east-2a"
+  availability_zone           = "eu-west-3a"
   associate_public_ip_address = true
   monitoring                  = true
-  key_name                    = aws_key_pair.class2.key_name
+  key_name                    = aws_key_pair.test_key.key_name
   user_data                   = file("user_data.sh")
   tags = {
     Name      = "Web ${count.index + 1}"
