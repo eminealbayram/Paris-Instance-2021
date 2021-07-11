9e5d33487907a76e14bfd00b38dc7646d21d3aea
eminealbayram <albayramemine27@gmail.com>
2021-07-11 21:09:04 +0000

    updated variables

--- TFVars-Example/provider.tf
+++ TFVars-Example/provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 resource "aws_key_pair" "chocolate" {
-  key_name   = "chocolate"
-  public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = var.key_name #allows people to create key with customized key_name
+  public_key = file(var.public_key)
 }
--- TFVars-Example/terraform.auto.tfvars
+++ TFVars-Example/terraform.auto.tfvars
@@ -1,1 +1,3 @@
-region = "eu-west-3"
+region     = "eu-west-3"
+key_name   = "chocolate"
+public_key = "~/.ssh/id_rsa.pub"
