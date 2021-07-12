a356d210a62b9f350d3c5dc7398bf7a4c8211f90
eminealbayram <albayramemine27@gmail.com>
2021-07-12 16:55:29 +0000

    added som more changes

--- MODULE/ec2.tf
+++ MODULE/ec2.tf
@@ -3,5 +3,6 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon.id
   instance_type = var.instance_type
   key_name      = aws_key_pair.chocolate.key_name
+  tags       = var.key_tags
 }
 
--- MODULE/emine.tfvars
+++ MODULE/emine.tfvars
@@ -1,7 +1,8 @@
 region        = "eu-west-3"
 key_name      = "emine"
 instance_type = "t3.micro"
-public_key    = "~/.ssh/id_rsa.pub"
+region = "eu-west-3"
+public_key = "~/.ssh/id_rsa.pub"
 
 
 key_tags = {
