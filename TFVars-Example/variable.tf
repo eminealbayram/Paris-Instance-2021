f13668712cd34b4b96604fa883b18e8e1ca45ddf
eminealbayram <albayramemine27@gmail.com>
2021-07-12 15:48:09 +0000

    added some comments regarding default region

--- TFVars-Example/variable.tf
+++ TFVars-Example/variable.tf
@@ -1,6 +1,11 @@
 variable "region" {
     type = string
     description = "Please provide your region"
+    #default = "eu-west-1" 
+    #however, tfvars overwrites it if we do not delete it in tfvars.
+    #So when we add default, and we remove region in tfvars, it will take from variable.tf
+    #If we don't need default, then region should be stated in tfvars.
+    
 }
 variable "key_name" {
     type = string
